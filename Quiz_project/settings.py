0ead65ad67edde76383b1705ef00b54dc7d4ad85
ahm186 <ali.h.almarhoun@gmail.com>
2021-05-06 12:07:36 +0300

    working on base and home page

--- Quiz_project/settings.py
+++ Quiz_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Quiz_project'
+    'Quiz_project',
+    'quiz',
+    'question',
+    'result'
 ]
 
 MIDDLEWARE = [
