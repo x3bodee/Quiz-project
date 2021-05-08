b96bc58e086837683b19808f834a97bf51fed495
ahm186 <ali.h.almarhoun@gmail.com>
2021-05-08 08:35:32 +0300

    making the login page made by Ali

--- Quiz_project/urls.py
+++ Quiz_project/urls.py
@@ -20,5 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('quiz.urls') ),
     # path('' , include('question.urls') ),
+    path('user/' , include('user.urls'))
     
 ]
---
+++ user/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from django.contrib.auth import views as as_view
+
+urlpatterns = [
+    path('login/' , as_view.LoginView.as_view(template_name = "index.html"))
+    
+    
+]
